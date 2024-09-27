ec55da568de40ea46e80a3f10f3e2c902e5ffbdb
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-09-27 16:35:29 +0500

    delete function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // yaha todos ki input form field or button bane ga
 
 import React, { useRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { addTodo } from './Config/Reducers/todoSlice';
+
+import { addTodo, removeTodo } from './Config/Reducers/todoSlice';
+import { useDispatch,useSelector } from 'react-redux';
 
 const App = () => {
 const dispatch = useDispatch();
@@ -17,6 +18,14 @@ function addToRedux(event){
     dispatch(addTodo({
             inputValue: todoVal.current.value
           }))
+
+}
+
+
+function deleteTodo(index){
+dispatch(removeTodo({
+  index:index
+}))
 }
 
 
@@ -26,6 +35,11 @@ function addToRedux(event){
     <input type="text"  placeholder='Enter Todos...' className='text-xl border-[1px] p-1 rounded-md w-full' ref={todoVal}/>
     <button className='text-xl border-[1px] rounded-md p-1 w-fit mt-2' onClick={addToRedux}>Add Todo</button>
 </form>
+{selector.length > 0 ? selector.map((item,index)=>{
+  return <div key={index}>
+    <li>{item.title}<button className='border-[1px] border-primary ml-2' onClick={() =>deleteTodo(index)}>Delete</button></li>
+  </div>
+}):<h1>No data found.</h1>}
     </>
   )
 }
--- src/Config/Store/store.js
+++ src/Config/Store/store.js
@@ -1,7 +1,7 @@
 // yaha global varible bane ga jis ma sara data store hoga
 import { configureStore } from "@reduxjs/toolkit";
 import todoSlice from "../Reducers/todoSlice"; 
-import cartReducer from "../reducers/cartSlice";
+
 
 
 export const store = configureStore({
